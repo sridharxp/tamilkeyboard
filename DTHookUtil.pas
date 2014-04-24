0c543c088edf418bb8b2fb616af6b8b1362e2e95
sridharxp <aurosridhar@gmail.com>
2014-04-24 09:20:10 +0530

    Updated with License

--- DTHookUtil.pas
+++ DTHookUtil.pas
@@ -1,3 +1,21 @@
+(* 
+Copyright (C) 2014, Sridharan S
+
+This file is part of Tamil Keyboard.
+
+Tamil Keyboard is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Tamil Keyboard is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License version 3
+ along with Tamil Keyboard.  If not, see <http://www.gnu.org/licenses/>.
+*)
 unit DTHookUtil;
 
 interface
