a995b20b85ce17e206891c049bda8cc18ee69405
sridharxp <aurosridhar@gmail.com>
2014-06-03 08:33:28 +0530

    Tested moving repository

--- DTHookUtil.pas
+++ DTHookUtil.pas
@@ -13,8 +13,8 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License version 3
- along with Tamil Keyboard.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License version 3 
+along with Tamil Keyboard.  If not, see <http://www.gnu.org/licenses/>.
 *)
 {
 Library to hangle Map logic
